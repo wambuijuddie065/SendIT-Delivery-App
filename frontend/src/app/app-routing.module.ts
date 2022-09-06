40a4855e92d68e8509f2283e37878b736868f39b
Judy Wambui <wambuijuddie065@gmail.com>
2022-09-06 17:20:05 +0300

    implemented page not found

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './Modules/shared/about/about.component';
 import { HomepageComponent } from './Modules/shared/homepage/homepage.component';
+import { PageNotFoundComponent } from './Modules/shared/page-not-found/page-not-found.component';
 
 const routes: Routes = [
 {path:'',redirectTo:'home',pathMatch:'full'},
 { path: 'home', component: HomepageComponent },
-{path:'auth',loadChildren:()=>import('./Modules/auth/auth.module').then(m=>m.AuthModule)}];
+{path:'about',component:AboutComponent},
+{path:'auth',loadChildren:()=>import('./Modules/auth/auth.module').then(m=>m.AuthModule)},
+{path:'**',component:PageNotFoundComponent}];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
