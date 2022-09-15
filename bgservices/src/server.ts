447109009e76a1a42d7764eb7ca320b0d6a484a1
Judy Wambui <wambuijuddie065@gmail.com>
2022-09-15 18:18:26 +0300

    bgservices folder structure

---
+++ bgservices/src/server.ts
@@ -0,0 +1,18 @@
+import express, { NextFunction, Request, Response } from "express";
+import cron from "node-cron";
+
+const app = express();
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.json({ Error: err.message });
+});
+const run = () => {
+  cron.schedule("*/5 * * * * *", async () => {
+    console.log("running in the background every 5 seconds");
+  });
+};
+run();
+
+app.listen(4003, () => {
+  console.log("App is running");
+});
